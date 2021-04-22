f7c650fff4490c0a63c0a0428ece250b2f235342
sander102907 <s.d.bruin@hotmail.com>
2021-04-22 21:13:27 +0200

    update tokenizer to clear and merge with other tokenizers

--- cpp_ast_parser/tokenizer.py
+++ cpp_ast_parser/tokenizer.py
@@ -10,8 +10,11 @@ class Tokenizer:
 
         # Tokenizer can also be used as counter 
         # Instead of tokenizing, count how many times each label occurs
+        # tokenizer should then be set to False
         self.tokenized = tokenized
 
+
+    # Returns the token from a label, creates tokens for new labels
     def get_token(self, label):
         if label in self.token_dict:
             if self.tokenized:
@@ -28,8 +31,9 @@ class Tokenizer:
             else:
                 self.token_dict[label] = 1
                 return label
-                
 
+                
+    # Return the label from a token
     def get_label(self, token):
         if self.tokenized:
             if token in self.label_dict:
@@ -39,6 +43,8 @@ class Tokenizer:
         else:
             return token
 
+
+    # Saves tokenizer to file
     def save(self):
         if not self.out_path:
             return
@@ -48,6 +54,8 @@ class Tokenizer:
         f.write(json_f)
         f.close()
 
+    
+    # Loads tokenizer from file
     def load(self, path):
         with open(path, 'r') as json_f:
             json_data = json_f.read()
@@ -56,3 +64,25 @@ class Tokenizer:
 
         # Reverse the token dict to create a label dict
         self.label_dict = dict((reversed(item) for item in self.token_dict.items()))
+
+
+    # Clears the tokenizer
+    def clear(self):
+        self.token_dict.clear()
+        self.label_dict.clear()
+
+
+    # Merges this tokenizer with another tokenizer
+    def merge(self, tokenizer: Tokenizer):
+        if self.tokenized:
+            for label in tokenizer.token_dict.keys():
+                if label not in self.token_dict:
+                    token = len(self.token_dict)
+                    self.token_dict[label] = token
+                    self.label_dict[token] = label
+        else:
+            for k,v in tokenizer.token_dict.items():
+                if k in self.token_dict:
+                    self.token_dict[k] += v
+                else:
+                    self.token_dict[k] = v
